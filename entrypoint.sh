bdd33276673021463ead546543a838a6317ae631
ronaregen <ronaregen@gmail.com>
2026-01-01 17:45:09 +0700

    change env to host

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,20 @@
 mkdir -p resources/svg
 
 # Login ke Infisical dan ambil token
-export INFISICAL_TOKEN=$(infisical login \
-  --method=universal-auth \
-  --client-id=$INFISICAL_MACHINE_CLIENT_ID \
-  --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET \
-  --domain=$INFISICAL_DOMAIN \
-  --plain --silent)
+# export INFISICAL_TOKEN=$(infisical login \
+#   --method=universal-auth \
+#   --client-id=$INFISICAL_MACHINE_CLIENT_ID \
+#   --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET \
+#   --domain=$INFISICAL_DOMAIN \
+#   --plain --silent)
 
 
-eval $(infisical export \
-  --token $INFISICAL_TOKEN \
-  --projectId $INFISICAL_PROJECT_ID \
-  --env $INFISICAL_SECRET_ENV \
-  --domain $INFISICAL_DOMAIN \
-  --format dotenv | sed 's/^/export /')
+# eval $(infisical export \
+#   --token $INFISICAL_TOKEN \
+#   --projectId $INFISICAL_PROJECT_ID \
+#   --env $INFISICAL_SECRET_ENV \
+#   --domain $INFISICAL_DOMAIN \
+#   --format dotenv | sed 's/^/export /')
 
 
 # Jalankan perintah Laravel
